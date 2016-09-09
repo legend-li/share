94347d4051ac60631489af91edae8e0c33466191
yiqisweet <mynameislxmax@outlook.com>
2016-09-09 15:12:22 +0800

    Update async_loading.js

--- function/async_loading.js
+++ function/async_loading.js
@@ -14,28 +14,34 @@
 	 *		.number_div{width: 100px;}
 	 *		Js Script need async :  async="async". This Script don't need async.
 	 */
-	  //console.log('loading...');
+	    console.log('loading...');
 	  document.write("<div id='hide-wrap'></div><div style='margin-top : 25%;' id='wrap-loading'><div id='loading_id' class='home_loading'><span></span></div><div class='number_div'><p id='data-number'></p></div></div>")
 			var i = 0,j = 30,k = false,upTime="";
-			upTime = setInterval(function(){
+			 upTime = setInterval(function(){
 				document.getElementById('loading_id').getElementsByTagName('span')[0].style.width = i +"%";
 				document.getElementById('wrap-loading').getElementsByTagName('p')[0].innerHTML = i +"%";
 				console.log(i);
 				if(i >= 98){
 			    		i = 98;
-			    		return;
 			    }
 				i += 1;
 			},100);
       document.onreadystatechange = subSomething;
       function subSomething()
       {
-         //console.log(document.readyState);
+         console.log(document.readyState);
         if(document.readyState == "complete"){
-        //console.log('done...');
-            document.getElementById('loading_id').getElementsByTagName('span')[0].style.width =  100+"%";
-            document.getElementById('wrap-loading').getElementsByTagName('p')[0].innerHTML = 100 +"%";
-						clearInterval(upTime);
+        	    endTime = setInterval(function(){
+        	    		if(i == 100){
+        	    			clearInterval(upTime);
+        	    			clearInterval(endTime);
+        	    		}
+        	    		document.getElementById('loading_id').getElementsByTagName('span')[0].style.width = i +"%";
+				document.getElementById('wrap-loading').getElementsByTagName('p')[0].innerHTML = i +"%";
+        	    		i += 1;
+        	    		
+        	    },10)
+        		console.log('done...');
             			setTimeout(function(){
             		 	document.getElementById('hide-wrap').remove();
             			document.getElementById('wrap-loading').remove();
