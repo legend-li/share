66b1b8c24ff3001e99f2081e36a7b1492af0a5ad
yiqisweet <mynameislxmax@outlook.com>
2017-04-26 23:04:46 +0800

    日常更新

--- code/index.js
+++ code/index.js
@@ -39,7 +39,7 @@ cat.eat('fish');
 //出现的最多次数
 
 // var str = 'asdsfdasdsadsaddddddd';
-// var json = {};13345233223413
+// var json = {};133452332234134
 //
 // for(var i =0;i < str.length; i ++){
 //     if(!json[str.charAt(i)]){
