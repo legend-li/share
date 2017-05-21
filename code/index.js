877bdcb9c56d55bbcf6edba5729c8c2402e3b506
yiqisweet <mynameislxmax@outlook.com>
2017-05-21 22:55:16 +0800

    日常更新

--- code/index.js
+++ code/index.js
@@ -5,7 +5,7 @@
 
 
 
-'usc strict'
+'usc strict';
 //继承
 
 //
@@ -20,20 +20,20 @@ function Animal(name){
 //原型方法
 Animal.prototype.eat = function(food){
     console.log(this.name + '正在吃 '+ food)
-}
+};
 
 function Cat(){
 
-}
+};
 Cat.prototype = new Animal();
 Cat.prototype.name = 'cat';
-;;
+
 //Test Code
 
-var cat = new Cat();
+let cat = new Cat();
 console.log(cat.name);
-cat.sleep()
-cat.eat('fish')
+cat.sleep();
+cat.eat('fish');
 
 
 //出现的最多次数
@@ -82,7 +82,7 @@ cat.eat('fish')
 // var s = [1,2,3,4,4,4,4,5,5,5,2,2];
 //
 // for(var i=0,o={},tmp=[],count=0,l=s.length;i<l;i++){
-//     if(o[s[i]]){251133441
+//     if(o[s[i]]){2511334413
 //         count++
 //     }else{
 //         o[s[i]] =1;
