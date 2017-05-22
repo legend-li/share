1a00cf4c4c1ca44f59623a615c6ca69a9a0080cf
yiqisweet <mynameislxmax@outlook.com>
2017-05-22 22:56:25 +0800

    日常更新

--- code/index.js
+++ code/index.js
@@ -39,7 +39,7 @@ cat.eat('fish');
 //出现的最多次数
 
 // var str = 'asdsfdasdsadsaddddddd';
-// var json = {};223367332
+// var json = {};2233673321
 //
 // for(var i =0;i < str.length; i ++){
 //     if(!json[str.charAt(i)]){
